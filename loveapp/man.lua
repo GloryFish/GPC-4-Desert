ca70305b810af90440ce8d95a74e6b7e309d59a9
Jay Roberts <jay@gloryfish.org>
2011-03-18 00:14:02 -0400

    Add man stub

---
+++ loveapp/man.lua
@@ -0,0 +1,21 @@
+-- 
+--  man.lua
+--  desert
+--  
+--  Created by Jay Roberts on 2011-03-18.
+--  Copyright 2011 GloryFish.org. All rights reserved.
+-- 
+
+Man = class('Man')
+
+function Man:initialize(pos)
+  self.position = pos
+end
+
+
+function Man:update(dt)
+end
+
+
+function Man:draw()
+end
